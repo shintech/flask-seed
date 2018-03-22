4e5f9601bacd539c953ca179b110a0ae4446feac
Michael Prather <mike@skynet.shintech.ninja>
2018-03-22 16:00:40 +0000

    templates

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return '<h1>Hello</h1>'
-  
+@app.route('/hello/<name>')
+def index(name):
+  return render_template('index.html', name=name)
+
 if __name__ == '__main__':
+  app.debug = True
   app.run(host='0.0.0.0')
